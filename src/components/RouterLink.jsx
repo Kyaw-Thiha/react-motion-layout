f45e97b0749e784cc9986569030de0b4d0a054fc
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2024-05-05 21:02:51 +0630

    Upgraded the packages + Updated the react router dom

--- src/components/RouterLink.jsx
+++ src/components/RouterLink.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useCallback } from 'react';
-import { useHistory, useLocation, Link } from 'react-router-dom';
+import { userNavigate, useLocation, Link } from 'react-router-dom';
 
 import GlobalContext from '../utils/globalContext';
 import actions from '../state/actions';
@@ -9,48 +9,51 @@ import actions from '../state/actions';
  * Only if there is only one scene in the current screen
  */
 export default function RouterLink({ to, replace, className, target, style, children }) {
-  const { store, dispatch } = useContext(GlobalContext);
-  const history = useHistory();
-  const location = useLocation();
+    const { store, dispatch } = useContext(GlobalContext);
+    const location = useLocation();
+    const navigate = userNavigate();
 
-  const onClick = useCallback((e) => {
-    const destination = typeof to === 'function' ? to(location) : to;
-    if (store.screen && store.onExit) {
-      e.preventDefault();
+    const onClick = useCallback((e) => {
+        const destination = typeof to === 'function' ? to(location) : to;
+        if (store.screen && store.onExit) {
+            e.preventDefault();
 
-      const method = replace ? history.replace : history.push;
 
-      /**
-       * Search for scenes and group by screen name
-       */
-      const num = Object.keys(store.scenes)
-        .reduce((acc, view) => {
-          const { screenName } = store.scenes[view];
-          acc[screenName] = [...(acc[screenName] || []), view];
-          return acc;
-        }, {});
+            /**
+             * Search for scenes and group by screen name
+             */
+            const num = Object.keys(store.scenes)
+                .reduce((acc, view) => {
+                    const { screenName } = store.scenes[view];
+                    acc[screenName] = [...(acc[screenName] || []), view];
+                    return acc;
+                }, {});
 
-      if (num[store.screen] && num[store.screen].length === 1) {
-        dispatch({
-          type: actions.view.setExitView,
-          sceneName: num[store.screen][0],
-        });
-      }
+            if (num[store.screen] && num[store.screen].length === 1) {
+                dispatch({
+                    type: actions.view.setExitView,
+                    sceneName: num[store.screen][0],
+                });
+            }
 
-      method(destination);
-    }
-  }, [dispatch, history, location, replace, store.onExit, store.scenes, store.screen, to]);
+            if (replace) {
+                navigate(destination, { replace: true })
+            } else {
+                navigate(destination)
+            }
+        }
+    }, [dispatch, history, location, replace, store.onExit, store.scenes, store.screen, to]);
 
-  return (
-    <Link
-      to={to}
-      style={style}
-      target={target}
-      replace={replace}
-      onClick={onClick}
-      className={className}
-    >
-      {children}
-    </Link>
-  );
+    return (
+        <Link
+            to={to}
+            style={style}
+            target={target}
+            replace={replace}
+            onClick={onClick}
+            className={className}
+        >
+            {children}
+        </Link>
+    );
 }
